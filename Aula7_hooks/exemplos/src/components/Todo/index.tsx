632b15aebb222b9038b37aea919796d77c489a54
Leonardo Tumadjian <tumadjian@gmail.com>
2020-10-13 20:06:07 -0300

    Atualiza aula 7

---
+++ Aula7_hooks/exemplos/src/components/Todo/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// const Todo: FunctionComponent = (props) => {
+
+//     return (
+//         <ul>
+//             {props.children}
+//         </ul>
+//     );
+// };
+
+// interface Props {
+//     children: React.ReactNode;
+// }
+
+// function Todo(props: Props) {
+//     return (
+//         <ul>
+//             {props.children}
+//         </ul>
+//     );
+// }
+
+function Todo() {
+    return <ul></ul>;
+}
+
+export default Todo;
