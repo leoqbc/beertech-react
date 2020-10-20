32837943376896cb9016d0495ab3dda8b5ae3ccd
Leonardo Tumadjian <tumadjian@gmail.com>
2020-10-20 13:39:58 -0300

    Atualiza Aula 12

--- Aula12_router/router/src/App.tsx
+++ Aula12_router/router/src/App.tsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Link, Redirect, Route, Switch } from "react-router-dom";
 
-// import Home from "./components/Home";
+import Home from "./components/Home";
+
 // import Sobre from "./components/Sobre";
 // import Contato from "./components/Contato";
 // import Users from "./components/Users";
 
+const Sobre = lazy(() => import("./components/Sobre"));
+const Contato = lazy(() => import("./components/Contato"));
+const Users = lazy(() => import("./components/Users"));
+const NotFound = lazy(() => import("./components/NotFound"));
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/sobre">Sobre</Link>
+          </li>
+          <li>
+            <Link to="/contato">Contato</Link>
+          </li>
+          <li>
+            <Link to="/users/10">Pegando de Users um User de código 10</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+
+        <Suspense fallback={<section>Carregando...</section>}>
+          <Switch>
+            <Route exact path="/home" component={Home}></Route>
+            <Route exact path="/sobre" component={Sobre}></Route>
+            <Route exact path="/contato" component={Contato}></Route>
+            <Route exact path="/users/:id" component={Users}></Route>
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- Aula12_router/router/src/components/Users.tsx
+++ Aula12_router/router/src/components/Users.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function Users() {
-  const { id: userId } = useParams<{ id: string }>();
-
-  return <h1>Hello user identification: {userId} </h1>;
+  const { id } = useParams<{ id: string }>();
+  return <h1>Hello user identification: {id}</h1>;
 }
 
 export default Users;
